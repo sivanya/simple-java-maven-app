76064626e9cc6dd571961dd8264089f056193020
sivanya <43091790+sivanya@users.noreply.github.com>
2018-09-11 08:03:45 -0400

    Commit 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Hello Siva";
+    private final String message = "Hello World!";
 
     public App() {}
 
